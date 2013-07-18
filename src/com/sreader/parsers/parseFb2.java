6b7a4633987512b4ec593284f10ff2dcd5bebef3
Vladyslav Yarovyi <vladyslav.yarovyi@masterofcode.com>
2013-07-18 17:31:50 +0300

    SReaderTest

---
+++ src/com/sreader/parsers/parseFb2.java
@@ -0,0 +1,35 @@
+package com.sreader.parsers;
+
+import com.sreader.leksem.Book;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class parseFb2 {
+	public static void parseFb2File(Book book, String filename, String filedir){
+		File dir = new File(filedir);
+		File file = new File(dir,filename); 
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+
+	           try {
+	        SAXParser parser = factory.newSAXParser();
+	        SaxPars saxp = new SaxPars(book);
+
+
+				parser.parse(file, saxp);
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+}
